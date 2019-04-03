abd1bc3df92b33387b995ac22a318fc92b642e47
Abdullateef <yomlateef@yahoo.com>
2019-04-03 02:02:43 +0100

    completed countdown style and animation

--- src/components/featured/Countdown.js
+++ src/components/featured/Countdown.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
 
 class Countdown extends Component {
     state = {
@@ -7,43 +8,45 @@ class Countdown extends Component {
 
     render() {
         return (
-            <div className="countdown_wrapper">
-                <div className="countdown_top">Event starts in</div>
-                <div className="countdown_bottom">
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            160
+            <Slide left delay={1000}>
+                <div className="countdown_wrapper">
+                    <div className="countdown_top">Event starts in</div>
+                    <div className="countdown_bottom">
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                160
+                            </div>
+                            <div className="countdown_tag">
+                                Days
+                            </div>
                         </div>
-                        <div className="countdown_tag">
-                            Days
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                22
+                            </div>
+                            <div className="countdown_tag">
+                                Hrs
+                            </div>
                         </div>
-                    </div>
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            22
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                43
+                            </div>
+                            <div className="countdown_tag">
+                                Mins
+                            </div>
                         </div>
-                        <div className="countdown_tag">
-                            Hrs
-                        </div>
-                    </div>
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            43
-                        </div>
-                        <div className="countdown_tag">
-                            Mins
-                        </div>
-                    </div>
-                    <div className="countdown_item">
-                        <div className="countdown_time">
-                            23
-                        </div>
-                        <div className="countdown_tag">
-                            Sec
+                        <div className="countdown_item">
+                            <div className="countdown_time">
+                                23
+                            </div>
+                            <div className="countdown_tag">
+                                Sec
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Slide>
         );
     }
 }
